5c7712306984764e052d1b14d1fe2f89b5fce96a
Jefferson Almanza <jefferson.almanza@litethinking.com>
2024-07-19 21:33:40 -0500

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, options) {
+    const { isServer } = options;
+
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "remote2", // Cambia el nombre según la aplicación remota
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./Pokemon": "./src/components/Atomos/Pokemon2",
+        },
+        shared: {
+          react: {
+            singleton: true,
+          },
+          "react-dom": {
+            singleton: true,
+          },
+        },
+      })
+    );
+
+    return config;
+  },
+};
